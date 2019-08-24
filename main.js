c9636c88ac3a7c6727f1098c37743725af3944a3
leandrocodes <leaxviana140@gmail.com>
2019-08-23 23:45:33 -0400

    fixing some bugs on navbar

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ const links = document.querySelectorAll('.nav-links li')
 const nav = document.querySelector('.navbar')
 
 hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('open')
-  nav.classList.toggle('open')
-  links.forEach(link => {
-    link.classList.toggle('fade')
-  });
-});
+    if(navLinks.classList.contains('open')){
+        nav.classList.toggle('closing')
+        setTimeout(() => {
+            nav.classList.remove('closing')
+        }, 500);
+    }
+    navLinks.classList.toggle('open')
+    nav.classList.toggle('open')
+    links.forEach(link => {
+        link.classList.toggle('fade')
+    })
+})
